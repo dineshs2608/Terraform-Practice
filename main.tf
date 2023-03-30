2f20965a8d26089bc8d01834c9341a1f7571f192
dinesh <lousiferkrish@gmail.com>
2023-03-30 12:36:07 +0530

    update of tf version

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.48.0"
+      version = "=3.38.0"
   }
   }
   #Backend storage of state file
