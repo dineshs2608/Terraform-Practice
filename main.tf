71b13d51e26acce9b5df693b14ded26ff7e3eb3c
dinesh <lousiferkrish@gmail.com>
2023-03-30 13:12:38 +0530

    update of tags

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ terraform {
 #providerblock
 provider "azurerm" {
   features {}
-  subscrption_id = var.subscription_id
+  subscription_id = var.subscription_id
   tenant_id = var.tenant_id
   client_id = var.client_id
   client_secret = var.client_secret
